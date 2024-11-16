742d793e1d36bf6217c3e55f41ff4295f723f09b
Salah-023 <90417315+Salah-023@users.noreply.github.com>
2024-11-16 22:41:12 +0100

    Describe your changes here

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,65 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.dagger.hilt.gradle) // <-- Only keep this one
+    kotlin("kapt")
+}
+
+dependencies {
+    // Default
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.activity.compose)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.graphics)
+    implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.androidx.material3)
+    implementation(libs.androidx.runtime.livedata)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+    androidTestImplementation(libs.androidx.ui.test.junit4)
+    debugImplementation(libs.androidx.ui.tooling)
+    debugImplementation(libs.androidx.ui.test.manifest)
+
+    // Exercise 2
+    implementation(libs.androidx.lifecycle.viewmodel.ktx)
+    implementation(libs.androidx.lifecycle.extensions)
+    implementation(libs.androidx.navigation.compose)
+    implementation(libs.androidx.hilt.navigation.compose)
+    implementation(libs.dagger.hilt.core)
+    implementation(libs.dagger.hilt.android)
+    kapt(libs.dagger.hilt.compiler)
+    implementation(libs.kotlinx.serialization.json)
+
+    // Exercise 4
+    implementation(libs.kotlinx.coroutines.core)
+    implementation(libs.kotlinx.coroutines.android)
+    implementation(libs.moshi)
+    implementation(libs.moshi.adapters)
+    implementation(libs.retrofit)
+    implementation(libs.retrofit.moshi)
+
+    // Exercise 5
+    implementation(libs.androidx.paging.runtime)
+    implementation(libs.androidx.paging.compose)
+    implementation(libs.coil)
+    implementation(libs.coil.compose)
+
+    // Exercise 6
+    implementation(libs.androidx.datastore.preferences)
+
+// Jetpack Compose
+    implementation( "androidx.compose.ui:ui:1.7.5")
+    implementation ("androidx.compose.material:material:1.7.5") // Required for BottomNavigation
+    implementation ("androidx.compose.ui:ui-tooling-preview:1.7.5")
+    implementation (libs.androidx.navigation.compose.v240alpha10)
+
+    // Required for Compose (do not remove)
+    implementation (libs.androidx.runtime.livedata.v105)
+    implementation (libs.androidx.lifecycle.runtime.ktx.v240)
 }
 
 android {
